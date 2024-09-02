db8f4b3580bcaeb5cd569aa73512a466c7c3340a
Dhanesh Raj P M <dhaneshrajpm@Dhaneshs-MacBook-Air.local>
2024-09-02 13:25:46 +0530

    backend package changes

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ mongoose.connect(process.env.MONGO_DB_URI)
   .then(() => console.log('Connected to DB'))
   .catch((err) => console.log(err));
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
