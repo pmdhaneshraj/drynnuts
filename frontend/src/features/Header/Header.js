5748de842c9c7030aa44e044542adeae116a5dc9
Dhanesh Raj P M <dhaneshrajpm@Dhaneshs-MacBook-Air.local>
2024-09-14 22:09:09 +0530

    styles config

--- frontend/src/features/Header/Header.js
+++ frontend/src/features/Header/Header.js
@@ -1,19 +1,20 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 
 import styles from './Header.module.scss'
-import LogoSvg from '../../assets/svg/logo.svg'
-import NavLinks from '../../components/NavLinks';
-import Menu from '../../components/Menu/Menu';
-import CartIcon from '../../components/CartIcon';
+import NavLinks from 'components/NavLinks';
+import Menu from 'components/Menu/Menu';
+import CartIcon from 'components/CartIcon';
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <div className={styles.container}>
       <div className={styles.leftContent}>
         <div className={styles.logo}>
-          <img className={styles.img} src={LogoSvg} alt='logo' />
-          {/* <span className={styles.title}>DRY 'N' NUTS</span> */}
+          <span className={styles.logo} onClick={() => navigate('/')}>D.N.N</span>
         </div>
         <NavLinks />
       </div>
